03f92b31aeffffe5e571bf30e1f399aa7f3fad69
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-08 08:57:42 +0500

    feat: wrapps body wrapper by context

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import AppWrapper from "src/wrappers/AppWrapper";
 import BodyWrapper from "src/wrappers/AppWrapper/BodyWrapper/BodyWrapper";
+import JsonOjectsArrayContextProvider from "./contexts/JsonOjectsArrayContext";
 
 render(
   <AppWrapper>
-    <BodyWrapper />
+    <JsonOjectsArrayContextProvider>
+      <BodyWrapper />
+    </JsonOjectsArrayContextProvider>
   </AppWrapper>,
   document.getElementById("root") as HTMLElement
 );
--- src/wrappers/AppWrapper/BodyWrapper/BodyWrapper.tsx
+++ src/wrappers/AppWrapper/BodyWrapper/BodyWrapper.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { routes } from "src/config/routes";
+import RendererPage from "src/pages/RendererPage";
+import UploadPage from "src/pages/UploadPage";
 
 function BodyWrapper() {
   return (
     <BrowserRouter>
-      <Switch></Switch>
+      <Switch>
+        <Route path={routes.uploadPage} exact component={UploadPage} />
+        <Route path={routes.rendererPage} exact component={RendererPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
